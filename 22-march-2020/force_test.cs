e805459a520e7861add97a80468b6646b4a65cde
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:41:43 +0530

    30th Program

---
+++ 22-march-2020/force_test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculate_force;
+namespace force_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void test_nikita()
+        {
+            force cf = new force();
+            float result = cf.nikita(20 , 10);
+            Assert.AreEqual(result, 200);
+
+        }
+    }
+}
