36511b214a1f8f6ecbada5b374ee8eca00059021
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 19:40:35 +0530

    3rd Program

---
+++ 22-march-2020/2.online_testing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.online_testing
+{
+    /*Write a program in C# Sharp to create a user define function with parameters. Test Data :
+    Please input a name : John
+    Expected Output :
+    Welcome friend John !
+    Have a nice day!
+    */
+   public class Program
+    {
+        static void Main(string[] args)
+     {
+            Console.WriteLine("Enter your Name : ");
+            string str = Console.ReadLine();
+            Program obj = new Program();
+            string name=obj.nice(str);
+            Console.WriteLine(name);
+            Console.ReadKey();
+            }
+            public string nice(string s)
+            {
+                Console.WriteLine("Welcome Friend ! " + s);
+                string str1="Have a Nice Day";
+            return str1;
+            }
+        }
+    }
+ 
