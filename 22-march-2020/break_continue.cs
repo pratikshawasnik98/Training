376cb6db49c9ab44018e34e6ea239875d0ef4704
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:39:41 +0530

    26th Program

---
+++ 22-march-2020/break_continue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int i = 0;
+            while(true)
+            {
+                Console.WriteLine(i);
+                i++;
+
+                if (i < num)
+                    continue;
+                else
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
