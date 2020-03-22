92719c8205cfc5c46403d0a09cd853c80d509d3b
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:25:36 +0530

    16th Program

---
+++ 22-march-2020/9.prime_number_test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _9.prime_number;
+namespace _9.prime_number_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program obj = new Program();
+            int res = obj.prime(31);
+            int actual = 0;
+            Assert.AreEqual(res, actual);
+        }
+    }
+}
