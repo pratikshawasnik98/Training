79d78cb181138134a994ff97b9120596359ff839
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 19:41:20 +0530

    4th Program

---
+++ 22-march-2020/2.online_testing_test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2.online_testing;
+namespace _2.online_testing_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            Program obj = new Program();
+            string name = obj.nice("Welcome Friend ! " );
+            
+            string actual = "Have a Nice Day";
+           
+            Assert.AreEqual(actual, name);
+        }
+    }
+}
