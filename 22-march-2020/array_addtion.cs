5b668a336db39c80a540717366163e28f0391b78
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:36:34 +0530

    22th Program

---
+++ 22-march-2020/array_addtion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_addtion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int sum = 0;
+                int[] num = new int[4];
+                Console.WriteLine("Enter a array element");
+                // num[4] = int.Parse(Console.ReadLine());
+                for (int i = 0; i <= 3; i++)
+                {
+                    num[i] = int.Parse(Console.ReadLine());
+                    //Console.(num[i]);
+                }
+
+                Console.WriteLine("Printed array elemnets are :");
+
+                foreach (int i in num)
+                {
+                    Console.WriteLine(i);
+                }
+                for (int i = 0; i <= 3; i++)
+                {
+                    // num[i] = int.Parse(Console.ReadLine());
+                    //Console.(num[i]);
+                    sum = sum + num[i];
+                }
+                Console.WriteLine("sum = " + sum);
+            
+            Console.ReadKey();
+        }
+
+    }
+}
