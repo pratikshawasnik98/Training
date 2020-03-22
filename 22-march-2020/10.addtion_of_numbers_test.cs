41b35d583864c13614a8a203fcc0a51090062406
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:33:08 +0530

    18th Program

---
+++ 22-march-2020/10.addtion_of_numbers_test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _10.addtion_of_numbers;
+namespace _10.addtion_of_numbers_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program obj = new Program();
+            int sum = obj.separate(1234);
+            int actual = 10;
+            Assert.AreEqual(sum, actual);
+        }
+    }
+}
