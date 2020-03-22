cbaf129f71c579ba170f257422c2f770708c6956
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 19:42:08 +0530

    6th Program

---
+++ 22-march-2020/3.online_testing_test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3.online_testing;
+namespace _3.online_testing_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+          //  Program obj = new Program();
+            int res = Program.sum(10,15);
+            int actual =25;
+            //string expected = res;
+            Assert.AreEqual(actual, res);
+        }
+    }
+}
