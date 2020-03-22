999d99db8479cea420ea8e900d3eaea2936860e6
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:22:06 +0530

    14th Program

---
+++ 22-march-2020/8.online.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.online
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("enter number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program();
+            obj.fibo(num);            
+            Console.ReadKey();
+        }
+        public int fibo(int r)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            while (c<r)
+            {
+                c = a + b;
+                if(c<r)
+                {
+                    Console.WriteLine(c);
+                }
+                a = b;
+                b = c;
+            }
+            return 0;
+        }
+    }
+}
