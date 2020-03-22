f3453600925689dfa7d76767e820a6f1d8e9bc0d
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:24:32 +0530

    13th Program

---
+++ 22-march-2020/8.online_test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _8.online;
+namespace _8.online_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program obj = new Program();
+            int res = obj.fibo(5);
+            int actual = 0;
+            Assert.AreEqual(res, actual);
+        }
+    }
+}
