74be1dfeb84541b7b2197e6a007476d6e1e45c36
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 19:43:40 +0530

    10th Program

---
+++ 22-march-2020/7.online_testing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _7.online;
+namespace _7.online_testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program obj = new Program();
+            int res = obj.power(3, 2);
+            int actual = 9;
+            int expected = res;
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
