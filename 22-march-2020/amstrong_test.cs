262293441353aa33bef0ad81eb57375d0b235237
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:35:57 +0530

    21th Program

---
+++ 22-march-2020/amstrong_test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using amstrong;
+namespace amstrong_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program p = new Program();
+            int res = p.ams(1234);
+            int actual = 4321;
+            int expected = res;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
