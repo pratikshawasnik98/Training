675f99b233d40a4f7e9826f48092f3e29ccc4491
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:25:08 +0530

    15th Program

---
+++ 22-march-2020/9.prime_number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.prime_number
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int k = 0, i = 1;
+            while (i <= num)
+            {
+                if (num % i == 0)
+                {
+                    k = k + 1;
+                }
+                i++;
+            }
+            Program obj = new Program();
+            obj.prime(k);
+            Console.ReadKey();
+        }
+
+        public int prime(int n)
+        { 
+            if(n==2)
+            {
+                Console.WriteLine(" Prime Number");
+            }
+           else
+                Console.WriteLine(" Not Prime Number");
+            return 0;
+        }
+    }
+}
