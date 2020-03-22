a017b89f063a10e0526b460bc661e437657ddc26
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:33:53 +0530

    19th Program

---
+++ 22-march-2020/add.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,square;
+            Console.WriteLine("Enter a two  number");
+             num = Convert.ToInt16(Console.ReadLine());
+
+           // num = Console.ReadLine();
+            square =  num * num;
+
+            Console.WriteLine("Answer = " +square);
+
+            Console.ReadKey();
+        }
+    }
+}
