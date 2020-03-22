85cf0af58562fa06feb1e0ed54a88c5668e2f970
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:21:31 +0530

    12th Program

---
+++ 22-march-2020/2D_array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] name = new string[5, 2];
+
+            for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<2;j++)
+                {
+                    Console.WriteLine("Enter Name and year");
+                    name[i, j] = Console.ReadLine();
+
+                }
+            }
+            //dispaly
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(name[i,j] + " ");
+                    //name[i, j] = Console.ReadLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
