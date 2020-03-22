9f008e942ea0d59c40ababa9da60d61357004bfb
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:40:12 +0530

    27th Program

---
+++ 22-march-2020/cal_area_circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cal_area_circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a radius");
+            int r = Convert.ToInt32(Console.ReadLine());
+            float res = 3.14f * r * r;
+            Console.WriteLine("result = " + res);
+            Console.ReadKey();
+        }
+    }
+}
