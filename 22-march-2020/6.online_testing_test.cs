f0a888057eca216e6df05553310596bec609373b
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 19:42:54 +0530

    8th Program

---
+++ 22-march-2020/6.online_testing_test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _6.online_testing;
+namespace _6.online_testing_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program p = new Program();
+            int res = p.first(10);
+            int sol = p.second(20);
+            int actual = sol;
+            //int excepted = res;
+            int except = res;
+            Assert.AreEqual(actual, except);
+        }
+    }
+}
