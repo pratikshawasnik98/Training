970c9b1afe412058fa7292d9f61cbcccaa6bb15f
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:40:43 +0530

    28th Program

---
+++ 22-march-2020/calculate_force.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculate_force
+{
+    public class force
+    {
+        public float nikita(float m , float a )
+        {
+            
+            //Console.WriteLine("enter a mass & acclerator");
+            //int m = Convert.ToInt32(Console.ReadLine());
+            //int a = Convert.ToInt32(Console.ReadLine());
+            float f = m * a;
+            // Console.WriteLine("result = " + f);
+            return f;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a=20, b=10;
+            force f = new force();
+            float res=f.nikita(a,b);
+            Console.ReadKey();
+        }
+    }
+}
