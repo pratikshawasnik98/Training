e33308e3db8fd0b2dec9dec78a681c4328cb47f8
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:38:39 +0530

    25th Program

---
+++ 22-march-2020/ascii_value.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ascii_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a  character");
+            int ch = Console.Read();
+            Console.WriteLine("ascii value = " + ch);
+            Console.WriteLine("character " + Convert.ToChar(ch));
+            Console.ReadKey();
+        }
+    }
+}
