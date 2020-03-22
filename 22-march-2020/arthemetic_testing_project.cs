e95a8deee7f795da95e9bd9a01cfd33c46fe848a
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:38:01 +0530

    24th Program

---
+++ 22-march-2020/arthemetic_testing_project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using arthemetic_testing;
+
+namespace arthemetic_testing_project
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void add_TestMethod1()
+        {
+            BasicMethod bm = new BasicMethod();
+            double res = bm.add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        public void subtract_TestMethod1()
+        {
+            BasicMethod bm = new BasicMethod();
+            double res = bm.subtract(20, 10);
+            double actual = 10;
+            double expected = res;
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
