829bdf0bf99be007020f8c564ef27a1ba04ab6e3
pratikshawasnik98 <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-22 21:34:27 +0530

    20th Program

---
+++ 22-march-2020/amstrong.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amstrong
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int d,rev=0;
+            while(num>0)
+            {
+                d = num % 10;
+                num = num / 10;
+                rev = rev * 10 + d;
+            }
+            Program p = new Program();
+            int result=p.ams(rev);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        public int ams(int r)
+        {
+            int rev;
+            rev = r;
+            return rev;
+        }
+    }
+}
