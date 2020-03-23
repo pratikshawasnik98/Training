1fd4daf542fbb5af4227f5cfa29d8a6353540b6e
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:43:40 +0530

    28th Program

---
+++ 23-march-2020/throw_exception.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace throw_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                float num1 = float.Parse(Console.ReadLine());
+                float num2 = float.Parse(Console.ReadLine());
+                float result=0;
+                result = num1 / num2;
+                //Console.WriteLine("Result = " + result);
+                //throw wrong = new wrong();
+                throw new Exception("result = " + result);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Final output ");
+            }        
+            Console.ReadKey();
+        }
+    }
+}
+
