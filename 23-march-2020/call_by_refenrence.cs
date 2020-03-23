7d9c284b6066c30de80da81866d317da3ea66408
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:33:44 +0530

    3rd Program

---
+++ 23-march-2020/call_by_refenrence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace call_by_refenrence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            float result;
+            result=add(num1,num2);
+            result = add(num1, num2,num3);
+            Console.Write("Result = " + result);
+            Console.ReadKey();
+
+        }
+        static int add(int a , int b)
+        {
+            return a + b;
+        }
+        static float add(int a, float b)
+        {
+            return a + b;
+        }
+        static float add(int a, float b,int c)
+        {
+            return a + b;
+        }
+    }
+}
