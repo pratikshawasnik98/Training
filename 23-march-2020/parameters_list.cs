a5d46a4a072b7fa1d28ba7f5ecd35ad680df1886
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:40:26 +0530

    20th Program

---
+++ 23-march-2020/parameters_list.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parameters_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a array element");
+            int[] num = new int[10];
+            int[] b = new int[10];
+
+            for (int i=0;i<=5;i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            b=show(num);
+
+            for (int i = 0; i <= 5; i++)
+            {
+                //b[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Result :" + b[i]);
+
+            }
+            Console.ReadKey();
+        }
+        static int[] show(params int [] a)
+        {
+            for(int i =a.Length;i>=0;i--)
+            {
+                Console.WriteLine("Result :" + a[i]);
+            }
+            return a;
+
+        }
+    }
+}
