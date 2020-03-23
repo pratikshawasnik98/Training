252eb015584e4a64881ed43c614e2980dc82fee4
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:39:14 +0530

    17th Program

---
+++ 23-march-2020/if_else_greater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_greater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a three numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1>num2 && num1>num3)
+            {
+                Console.WriteLine("greater number = " + num1);
+            }
+            else if(num2>num3)
+            {
+                Console.WriteLine("greater number = " + num2);
+            }
+            else
+            {
+                Console.WriteLine("greater number = " + num3);
+            }
+            Console.ReadKey();
+        }
+    }
+}
