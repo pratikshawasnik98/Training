5af785cd35c32c997f63bb882aafab6b839b3b40
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:38:57 +0530

    16th Program

---
+++ 23-march-2020/goto_statement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goto_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+                goto even;
+                Console.WriteLine("Odd");
+            goto end;
+            even:
+                Console.WriteLine("even");
+            end:;
+                Console.ReadKey();
+        }
+    }
+}
