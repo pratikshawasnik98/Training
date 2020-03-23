50a955825156b3c8529ee7b26c9d7ca18c24e8db
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:43:17 +0530

    27th Program

---
+++ 23-march-2020/structure_employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structure_employee
+{
+    class Program
+    {
+        struct employee
+        {
+            public string firstName;
+            public int age;
+        }
+        static void Main(string[] args)
+        {
+            employee companyEmployee;
+            Console.WriteLine("enter a name");
+            companyEmployee.firstName = Console.ReadLine();
+            Console.WriteLine("enter a age");
+            companyEmployee.age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("First Name = " + companyEmployee.firstName + " \n age = "+ companyEmployee.age);
+            //Console.WriteLine("age = " + companyEmployee.age);
+
+            Console.ReadKey();
+        }
+    }
+}
