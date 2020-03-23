d16259623c866e557b3d40d1b6c729028edb1d17
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:35:34 +0530

    7th Program

---
+++ 23-march-2020/create_enum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_enum
+{
+    class Program
+    {
+        enum grade { dist = 75, firist = 60, secound = 50 }
+        static void Main(string[] args)
+        {
+            grade g = grade.dist;
+            int g1 = Convert.ToInt32(g);
+            Console.WriteLine("grade= " + g1);
+
+            if(g1>=75)
+            {
+                Console.WriteLine("very good");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
