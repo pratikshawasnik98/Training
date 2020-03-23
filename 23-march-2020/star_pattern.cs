bcbb3a4f77c7ff46d9670d36e6a85b3d59069202
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:42:14 +0530

    24th Program

---
+++ 23-march-2020/star_pattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace star_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a row value");
+            int row = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < row; i++)
+            {
+                if (i == 1 || i == 3 || i==5 || i==7)
+                {
+                    continue;
+                }
+                for(int sp=row-1;sp>=0;sp--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("  *");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
