83a287083346c2d97132d35a186db959e44432ef
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:39:48 +0530

    19th Program

---
+++ 23-march-2020/new_class_method_calling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_class_method_calling
+{
+   class sample
+    {
+        public static int addition()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            return num1 + num2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            Console.WriteLine("Enter a two number");
+           
+            //sample s = new sample();
+            sum=sample.addition();
+            //sum =s.addition();
+            //int sum = num1 + num2;
+            Console.WriteLine("Result + " + sum);
+            Console.ReadKey();
+        }
+        
+    }
+}
+
