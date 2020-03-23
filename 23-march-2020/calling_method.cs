1ecd1bc79500e9d123a03b45125c93a04888edac
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:34:13 +0530

    4th Program

---
+++ 23-march-2020/calling_method.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calling_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            Console.WriteLine("Enter a two number");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            sum =addition(num1,num2);
+            //int sum = num1 + num2;
+            Console.WriteLine("Result + " + sum);
+            Console.ReadKey();
+        }
+        public static int addition(int n1,int n2)
+        {
+            
+            return n1 + n2;
+        }
+
+    }
+}
