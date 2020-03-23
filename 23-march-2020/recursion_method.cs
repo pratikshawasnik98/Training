82aad92fea08e068c95f1fa43b89785b8daa9eb4
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:41:40 +0530

    23rd Program

---
+++ 23-march-2020/recursion_method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion_method
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,result;
+            Console.WriteLine("enter number: ");
+            num = int.Parse(Console.ReadLine());
+            Program obj = new Program();
+            result = obj.factorial(num);
+            Console.WriteLine(result);
+            Console.ReadKey();      
+        }
+        public int factorial(int a)
+        {
+
+            int fact = 1;
+            //int i = 1;
+            while (a > 0)
+            {
+                fact = fact * a;
+                a--;
+            }
+            return fact;
+            
+        }
+
+    }
+}
