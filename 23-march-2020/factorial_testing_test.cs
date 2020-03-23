13f1e94ac2c67b0ad52cf4005d31c8220490f941
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:37:41 +0530

    14th Program

---
+++ 23-march-2020/factorial_testing_test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using factorial_testing;
+
+namespace factorial_testing_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            multiply obj = new multiply();
+            int result = obj.multi(2);
+            int actual = 4;
+            int expected = result;
+            Assert.AreEqual(expected, actual);
+           // return 0;
+        }
+    }
+}
