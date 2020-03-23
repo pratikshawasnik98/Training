ab7c61d997a96955ab940c95147bb467af046c2e
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:34:51 +0530

    5th Program

---
+++ 23-march-2020/class_object.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+            Console.WriteLine("Enter Employee Number and Name: ");
+            obj.EmpNo = Convert.ToInt32(Console.ReadLine());
+            obj.EmpName = Console.ReadLine();
+            Console.WriteLine("Employee Number = " + obj.EmpNo);
+            Console.WriteLine("Employee Name = " + obj.EmpName);
+            Console.ReadKey();
+
+        }
+    }
+    class Employee
+    {
+       public int EmpNo;
+       public string EmpName;
+
+    }
+}
