31ff84c3af53d526c53aaa8b747479eec216601c
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:36:44 +0530

    11th Program

---
+++ 23-march-2020/exxit_to_enter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exxit_to_enter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press a enter key to exit ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("  Press enter key");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
