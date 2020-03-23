86bae18b1362aa82a24bad17ec760ccdb4088485
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:36:09 +0530

    9th Program

---
+++ 23-march-2020/divide_testing_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using divide_testing;
+
+namespace Project_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            divide obj = new divide();
+            int res = obj.solution(10,2);
+            int actual = 5;
+            int expected = res;
+            Assert.AreEqual(actual, expected);
+
+        }
+    }
+}
