5424cc12752636a1029a8d4b3cf47e4bd5433ad3
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:39:29 +0530

    18th Program

---
+++ 23-march-2020/instance_class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instance_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//not excute3ted
+            //int i=7;
+            Program p = new Program();
+            Console.Write("i =" + i);
+            Console.WriteLine("Enter");
+            Console.ReadKey(p);
+            char i = Convert.ToInt64(Console.ReadKey(i));
+
+            //Console.ReadKey();
+        }
+    }
+}
