4c0dbf336b9cfad0e4c791257f64942d7547332c
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-23 23:31:18 +0530

    1st Program

---
+++ 23-march-2020/calculate_KE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculate_KE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a mass & velocitnm m y");
+            float m = Convert.ToSingle(Console.ReadLine());
+            float v = Convert.ToSingle(Console.ReadLine());
+            float ke = 0.5f * m * v * v;
+            Console.WriteLine("result = " + ke);
+            Console.ReadKey();
+        }
+    }
+}
