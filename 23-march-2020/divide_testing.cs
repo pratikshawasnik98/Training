11ade8730ec5d278bf01f914d55a38e9d61f5297
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:35:53 +0530

    8th Program

---
+++ 23-march-2020/divide_testing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace divide_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            divide obj = new divide();
+            obj.solution(100,2);
+            Console.ReadKey();  
+        }
+    }
+    public class divide
+    {
+        public int solution(int a,int b)
+        {
+            int res;
+            res = a / b;
+            Console.WriteLine(res);
+            return res;
+        }
+    }
+}
