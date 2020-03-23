d7d99f0824f3d7e6d2fc66207db8ddcb5dd36e7b
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:43:58 +0530

    29th Program

---
+++ 23-march-2020/try_catch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter a number");
+                int num2 = int.Parse(Console.ReadLine());
+                int result = num1 + num2;
+                Console.WriteLine("Result = " + result);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("End of code");
+            Console.ReadKey();
+        }
+    }
+}
