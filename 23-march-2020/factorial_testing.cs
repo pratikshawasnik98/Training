d187a79ed47cffe8b77d027f6c4d90c0b687526d
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-23 23:37:22 +0530

    13th Program

---
+++ 23-march-2020/factorial_testing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial_testing//.exe program namespane_name
+{
+    //with argument and with return type
+    public class multiply
+    {
+        public int multi(int n)
+        {
+            //int num=n;
+            int  fact;
+            fact = n * n;
+            Console.WriteLine("Result :" + fact);
+            return fact;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            multiply obj = new multiply();
+            obj.multi(4);
+            Console.ReadKey();
+        }
+    }
+}
