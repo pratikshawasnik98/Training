18fb91a7885f07ba57a7b24c363594b81e84c0f1
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:36:14 +0530

    18th Program

---
+++ 24-march-2020/pattern.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            for(int i=0;i<4;i++)
+            {
+                Console.Write(" * ");
+                for (int j = 1; j < 3; j++)
+                {
+                    if (i == 1 || i == 2 || i==3)
+                    {
+                        if (j == 1 || j == 2)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(" * ");
+                    }
+
+                }
+                    /*
+                    if(i==1 && i==2 && i==3)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(" * ");
+                    }
+                    for (int j = 1; j < 10; j++)
+                    {
+                        if (i == 2 || j == 2  || i==3 || j == 3)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write("*");
+                        }*/
+                    //    Console.Write(" % ");
+                    //}
+                    Console.WriteLine();
+            //}
+
+
+            for (int i = 0; i <4; i++)
+            {
+                Console.Write(" * ");
+                for (int j = 1; j <= 18; j++)
+                {
+                    //
+                    if (i == 1 || i == 2 || i == 3)
+                    {
+                        if (j == 1 || j == 2)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(" * ");
+                    }
+                    if (j == 4 || j==3)
+                    {
+                        Console.Write("*");
+                    }
+            }
+                Console.WriteLine();
+            }
+
+
+
+                Console.ReadKey();
+        }
+    }
+}
