e15ad63ddbab7fead03ffc9dfd518316f25de16c
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:28:07 +0530

    2sd Program

---
+++ 24-march-2020/area_perimeter_of_triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace area_perimeter_of_triangle
+{
+    class Triangle
+    {
+        public int side1 { get; set; }
+        public int side2 {get;set;}
+
+        public Triangle()
+        {
+            Console.WriteLine("Area of Trinagle :");
+            
+            Console.WriteLine("Enter a 1st side :");
+            side1 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter a 2sd side :");
+            side2 = int.Parse(Console.ReadLine());
+            int areaOfTriangle = (side1 * side2)/2;
+
+            Console.WriteLine("Area of Trinagle Result :"+areaOfTriangle);
+
+            Console.WriteLine("Perimeter of Trinagle :");
+            int PerimeterOfTriangle = 3+4+5;
+
+            Console.WriteLine("Area of Trinagle Result :" + PerimeterOfTriangle);            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+            Console.ReadKey();
+        }
+    }
+}
