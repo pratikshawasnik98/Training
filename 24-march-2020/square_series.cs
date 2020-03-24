2d94337799ad0696d023811f4eb161d785f26770
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:38:37 +0530

    24th Program

---
+++ 24-march-2020/square_series.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace square_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a last range ");
+            int range = int.Parse(Console.ReadLine());
+            int i;
+            for (i = 1; i < range; i++)
+            {
+                int num = i * i;
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
