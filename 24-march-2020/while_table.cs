cc8a620ed546c9dc7da97c9450faeec8a01c4136
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:41:20 +0530

    30th Program

---
+++ 24-march-2020/while_table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            //int fact;
+            int i=1;
+            while (num > 0)
+            {
+
+                fact = fact * num;
+                num--;
+            }
+            Console.WriteLine("Factorial  = " + fact);
+
+            Console.ReadKey();
+        }
+    }
+}
