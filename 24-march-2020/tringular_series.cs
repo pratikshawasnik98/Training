a93d6b3058c86990d0e5756c362653814ebff70e
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:39:57 +0530

    27th Program

---
+++ 24-march-2020/tringular_series.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tringular_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a last range ");
+            int range = int.Parse(Console.ReadLine());
+            int i,sum=0;
+            for (i = 1; i < range; i++)
+            {
+                sum = sum + i;
+                Console.WriteLine(sum);
+            }
+            Console.ReadKey();
+        }
+    }
+}
