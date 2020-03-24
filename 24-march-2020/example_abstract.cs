073455417d10dd62ccf38812a010722b26f4e972
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:32:15 +0530

    8th Program

---
+++ 24-march-2020/example_abstract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace example_abstract
+{
+    abstract class Account
+    {
+        public abstract void Deposite();
+        public void ShowBalance()
+        {
+            Console.WriteLine("Balanace");
+        }
+    }
+    class Saving : Account
+    {
+        public override void Deposite()
+        {
+            Console.WriteLine("Deposite");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Saving();
+            account.ShowBalance();
+            account.Deposite();
+            Console.ReadKey();
+        }
+    }
+}
