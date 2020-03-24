d9a52adc1d873127a934440db714591fc6973706
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:33:45 +0530

    12th Program

---
+++ 24-march-2020/inheritance_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace inheritance_1
+{
+    class BaseClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Parent Class");
+        }
+    }
+    class ChildClass : BaseClass
+    {
+        public void Display1()
+        {
+            Console.WriteLine("Child Class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass baseclass = new BaseClass();
+            baseclass.Display();
+
+            ChildClass childclass = new ChildClass();
+            childclass.Display1();
+            childclass.Display();
+            
+
+            Console.ReadKey();
+        }
+    }
+}
