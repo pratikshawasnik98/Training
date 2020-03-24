601bc73c0395fbf942f7d861cbaebfff854b7e2d
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:34:31 +0530

    14th Program

---
+++ 24-march-2020/inheritant_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace inheritant_2
+{
+    class BaseClass
+    {
+
+        private void Display()
+        {
+            Console.WriteLine("Parent Class");
+        }
+    }
+    class ChildClass : BaseClass
+    {
+        public void Display1()
+        {
+            Console.WriteLine("Child Class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass baseclass = new BaseClass();
+            baseclass.Display();//give error becuase its protection level
+
+            ChildClass childclass = new ChildClass();
+            childclass.Display1();
+            //childclass.Display();
+
+
+            Console.ReadKey();
+        }
+    }
+}
