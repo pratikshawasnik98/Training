9d4151136cd8c90ead3e2bd8dd33687ea5426bdd
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:37:38 +0530

    21th Program

---
+++ 24-march-2020/runtime_plymorphism.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace runtime_plymorphism
+{
+    class Figure
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Base Class");
+        }
+    }
+    class Rectangle : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Child Class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is create a child class object with base class name through we can call a base class method
+            Figure r = new Rectangle();
+            r.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
+
