4cc39cd955b45300f918b3364ae32026419bf07f
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:30:34 +0530

    5th Program

---
+++ 24-march-2020/BaseClassConstructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BaseClassConstructor
+{
+    class Token
+    {
+        protected  Token (String Name)
+        {
+            Console.WriteLine("Base Class Constructor = Name : " + Name);
+        }
+    }
+    class CommentToken:Token
+    {
+        public CommentToken(String Name): base(Name)
+        {
+            Console.WriteLine("Derived class Constructor + Name :" +Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommentToken cmttoken = new CommentToken("ABC");
+            Console.ReadKey();
+        }
+    }
+}
