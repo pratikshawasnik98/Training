f2654efe9190c4379c96c6d6bf9365d4ddf79e03
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:38:59 +0530

    25th Program

---
+++ 24-march-2020/StudentClass_to_print_constant_value.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StudentClass_to_print_constant_value
+{
+    class Student
+    {
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+
+        public void GetStudentDetail(int rollNumber, string name)
+        {
+            RollNumber = rollNumber;
+            Name = name;
+        }
+
+        public void DisplayResult()
+        {
+            Console.WriteLine("Student Roll number  : " + RollNumber);
+            Console.WriteLine("Student Name  : " + Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.GetStudentDetail(2, "John");
+            student.DisplayResult();
+            Console.ReadKey();        }
+    }
+}
