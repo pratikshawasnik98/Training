4d2c54986453eb30b39dda8f265d20a44d186af8
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:30:07 +0530

    4th Program

---
+++ 24-march-2020/arithmetic_series.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace arithmetic_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a last range ");
+            int range = int.Parse(Console.ReadLine());
+            int i;
+            for(i=1;i<range;i=i+3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
