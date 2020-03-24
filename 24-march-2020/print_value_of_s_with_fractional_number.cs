2dd171ea82ff62c24b7e7e4c9c7567d7b22aaacb
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:36:52 +0530

    19th Program

---
+++ 24-march-2020/print_value_of_s_with_fractional_number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace print_value_of_s_with_fractional_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float i = 1,s=0;
+            
+            for(i=1;i<=50;i++)
+            {
+                s =s+1 / i;
+                Console.WriteLine("Sum of s :{0} \ni : {1}  ", s, i);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
