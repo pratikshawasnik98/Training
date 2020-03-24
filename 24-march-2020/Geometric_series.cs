d8f610830c54e090115726e296432739acfc1300
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:32:59 +0530

    10th Program

---
+++ 24-march-2020/Geometric_series.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Geometric_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a last range ");
+            int range = int.Parse(Console.ReadLine());
+            int i;
+            for (i = 5; i < range; i = i *2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
