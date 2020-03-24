c5a62206d3beec8482b8b35145644c51bdb2dc43
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:31:28 +0530

    7th Program

---
+++ 24-march-2020/cube_series.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cube_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a last range ");
+            int range = int.Parse(Console.ReadLine());
+            int i;
+            for (i = 1; i < range; i++)
+            {
+                int num = i * i *i;
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
