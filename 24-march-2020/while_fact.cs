c94a98bdd76fc0b74227ff86eab67e2a33f9c2dc
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:40:57 +0530

    29th Program

---
+++ 24-march-2020/while_fact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_fact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int fact = 1;
+            while(num>0)
+            {
+                fact = fact * num;
+                num--;
+            }
+            Console.WriteLine("Factorial  = " + fact);
+
+            Console.ReadKey();
+        }
+    }
+}
