42f195d6df81f0ab215e705fb3c7b454977e8df9
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:32:41 +0530

    9th Program

---
+++ 24-march-2020/example_Interface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace example_Interface
+{
+    interface Account
+    {
+         void Deposite();
+    }
+    interface Account1 : Account
+    {
+        void ShowBalanace();
+    }
+    class Saving : Account1
+    {
+        public void ShowBalanace()
+        {
+            Console.WriteLine("Balanace");
+        }
+        public  void Deposite()
+        {
+            Console.WriteLine("Deposite");
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saving account = new Saving();
+            account.ShowBalanace();
+            account.Deposite();
+            Console.ReadKey();
+        }
+    }
+}
