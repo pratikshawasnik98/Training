6b06ed3bc817eb5afc88cc876b706242a8d2a842
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:37:21 +0530

    20th Program

---
+++ 24-march-2020/print_value_of_s_with_fractional_number_evenOdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace print_value_of_s_with_fractional_number_evenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                float i = 3, s = 0,j=2;
+            s = s + 1;
+            while (i<=8)
+                {
+                    s = s +( i / j);
+                    Console.WriteLine("\ni : {0}  \nj: {1} \nSum of s : {2}",i, j,s);
+                    i = i + 2;
+                    j = j * 2;
+                
+                 }            
+            Console.WriteLine("Sum of s : "+s);
+            Console.ReadKey();
+        }
+    }
+}
