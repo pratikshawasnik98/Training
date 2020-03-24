cd3b4b0dd5d9c57d9469d63952387a1952544062
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:35:37 +0530

    17th Program

---
+++ 24-march-2020/Multiple_Interface_By_Explicity_Calling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Multiple_Interface_By_Explicity_Calling
+{
+    //explicit Interface
+    interface IToken
+    {
+        void Name();
+    }
+    interface IVisitable
+    {
+        void Accept(String n);
+    }
+    class Token : IToken, IVisitable
+    {
+        void IToken.Name()
+        {
+            Console.WriteLine("Hello");
+            //return "Hello from name";
+        }
+        /*string IVisitable.Name()
+        {
+            Console.WriteLine("Hey");
+        }*/
+        void IVisitable.Accept(string n)
+        {
+            Console.WriteLine("Name " + n);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IToken token = new Token();
+            //Console.WriteLine(token.Name());
+            token.Name();
+
+            //token.Accept("ABC");
+            Console.ReadKey();
+        }
+    }
+}
