b052d2103168969246597aae4d26a811055f37aa
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-24 23:34:47 +0530

    15th Program

---
+++ 24-march-2020/method_Overriding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace method_Overriding
+{
+    class Figure
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Base Class");
+        }
+    }
+    class Rectangle : Figure
+    {
+        public new void Draw()
+        {
+            Console.WriteLine("Child Class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //this is create a child class object through we can call a child class method
+            //Rectangle r = new Rectangle();
+            //r.Draw();
+            
+
+            //this is create a child class object with base class name through we can call a base class method
+            Figure r = new Rectangle();
+            r.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
