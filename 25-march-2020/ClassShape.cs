f94d19e3bf9409fe7b568ffb12210ab843e80c22
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:09:25 +0530

    14th Program

---
+++ 25-march-2020/ClassShape.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassShape
+{
+    class Shape
+    {
+        public void ShowShape()
+        {
+            Console.WriteLine("This is Shape");
+        }
+    }
+    class Rectangle :Shape
+    {
+        public void ShowRectangle()
+        {
+            Console.WriteLine("This is Rectangular Shape");
+        }
+    }
+    class Circle : Shape
+    {
+        public void ShowCircle()
+        {
+            Console.WriteLine("This is circular Shape");
+        }
+    }
+    class Square : Rectangle
+    {
+        public void ShowSquare()
+        {
+            Console.WriteLine("Square is a Rectangle");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            
+            square.ShowShape();
+            square.ShowRectangle();
+
+            Console.ReadKey();
+        }
+    }
+}
