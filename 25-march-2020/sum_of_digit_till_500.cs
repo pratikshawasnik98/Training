b5bfb37ffcf1a2b29d40bc5fe6d0a16415f60986
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:17:20 +0530

    28th Program

---
+++ 25-march-2020/sum_of_digit_till_500.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sum_of_digit_till_500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number : ");
+            int num = int.Parse(Console.ReadLine());
+            int rem =0,sum = 0,num1,num2,num3;
+          if(num<=500)
+            {
+                num1 = num % 10;
+                num = num / 10;
+
+                num2 = num % 10;
+                num = num / 10;
+
+                num3 = num % 10;
+                num = num / 10;
+
+                sum = num1 + num2 + num3;
+            }
+            
+            Console.WriteLine("Sum of Digit is : " + sum);
+            Console.ReadKey();
+        }
+    }
+}
