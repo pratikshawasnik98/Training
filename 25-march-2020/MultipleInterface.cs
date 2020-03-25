1c6ef5dfd91404abb3040011f670a7674f321617
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:13:05 +0530

    22th Program

---
+++ 25-march-2020/MultipleInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MultipleInterface
+{
+    //implicity Interface
+    interface IToken
+    {
+        string Name();
+    }
+    interface IVisitable
+    {
+        void Accept(String n);
+    }
+    class Token:IToken,IVisitable
+    {
+        public string Name()
+        {
+            return "Hello from name";
+        }
+        public void Accept(string n )
+        {
+            Console.WriteLine("Name " + n);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Token token = new Token();
+            Console.WriteLine(token.Name());
+
+            token.Accept("ABC");
+            Console.ReadKey();
+        }
+    }
+}
