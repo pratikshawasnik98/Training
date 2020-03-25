e022a5581106f7f593d111da658b69c74b967f6d
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:05:25 +0530

    6th Program

---
+++ 25-march-2020/areaOf10Element.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace areaOf10Element
+{
+    class Square 
+    {
+        public float Side { get; set; }
+        //public float Side2 { get; set; }
+
+        public Square(float side)
+        {
+            Side = side;
+        }
+        public void areaSquare()
+        {
+            float areasquare = Side * Side;
+            Console.WriteLine("\nArea of Square :" + areasquare);
+        }
+        public void perimeterSquare()
+        {
+            float perimetersquare = 4 * Side;
+            Console.WriteLine("Perimeter of Square :" + perimetersquare);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            Square[] square = new Square[10];
+            for(int i = 0;i<10;i++)
+            {
+
+                Console.WriteLine("Entera side:");
+                float s = float.Parse(Console.ReadLine());
+                //square[i] = float.Parse(Console.ReadLine());
+                square[i] = new Square(s);
+                square[i].areaSquare();
+            }
+            
+            //square.perimeterSquare();
+
+            Console.ReadKey();
+        }
+    }
+}
