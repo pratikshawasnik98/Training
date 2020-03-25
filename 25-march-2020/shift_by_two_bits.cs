f74cc13702ac674b2ef53cebec25cb0134e05e0a
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:17:03 +0530

    27th Program

---
+++ 25-march-2020/shift_by_two_bits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace shift_by_two_bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 3;
+            c <<= 2;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
