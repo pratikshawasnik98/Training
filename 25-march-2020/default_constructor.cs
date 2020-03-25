d8240abf95188324107b0f7f3dfb5097495e2b69
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:10:04 +0530

    16th Program

---
+++ 25-march-2020/default_constructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace default_constructor
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            constant c = new constant();
+            Console.WriteLine("Account :"+c.actno);
+            Console.ReadKey();
+        }
+    }
+    class constant
+    {
+        public int actno;
+        public constant()
+        {
+            actno = 22;
+        }
+    }
+}
+
