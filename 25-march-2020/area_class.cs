4bfb866ec5c6155a9314168cee58f120af216a44
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:04:02 +0530

    4th Program

---
+++ 25-march-2020/area_class.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace area_class
+{
+    class Area
+    {
+        public float Length { get; set; }
+        public float Breadth { get; set; }
+        public float areaRectangle { get; set; }
+        public void SetDim(float lenght,float breadth)
+        {
+            Length = lenght;
+            Breadth = breadth;
+            //areaRectangle = lenght * breath;
+        }
+        public float getArea()
+        {
+            
+            areaRectangle = Length * Breadth;
+            return areaRectangle;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Length :");
+            float len = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a Breadth :");
+            float bre = float.Parse(Console.ReadLine());
+
+            Area area = new Area();
+            area.SetDim(len, bre);
+            Console.WriteLine(area.getArea());
+
+            Console.WriteLine();
+        }
+    }
+}
