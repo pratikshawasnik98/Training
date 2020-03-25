3850695770b5e77778199c0eed20f0b0a7b1aad4
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:12:27 +0530

    20th Program

---
+++ 25-march-2020/height_average_of_all_student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace height_average_of_all_student
+{
+    class Student
+    {
+        public int numberstudent;
+        public float averageHeight;
+        public float height1;
+
+        public void showDetail()
+        {
+            Console.WriteLine(numberstudent + "       " + averageHeight + "       ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float averageHeight=0;
+            int[] count = new int[3];
+            Student[] student = new Student[3];
+            for (int i = 0; i < 3; i++)
+            {
+                student[i] = new Student();
+                Console.WriteLine("Enter a roll number: ");
+                student[i].numberstudent = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter a Height of student: ");
+                student[i].height1 = int.Parse(Console.ReadLine());
+
+                averageHeight = (count[i]+student[i].height1) / 3;
+            }
+            Console.WriteLine(averageHeight +"        ");
+            Console.ReadKey();
+        }
+    }
+}
