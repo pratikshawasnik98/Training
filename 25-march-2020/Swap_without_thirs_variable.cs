531f444dbdfe793abf2dc795d3970b749551b3c5
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:17:35 +0530

    29th Program

---
+++ 25-march-2020/Swap_without_thirs_variable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Swap_without_thirs_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number 1 :");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number 2 :");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int res = 0;
+            res = num1;
+            num1 = num2;
+            num2 = res;
+
+            Console.WriteLine("Enter a number 1 :" + num1);
+            Console.WriteLine("Enter a number 2 :" + num2);
+
+            Console.ReadKey();
+        }
+    }
+}
