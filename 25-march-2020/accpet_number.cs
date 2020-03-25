093ffb06d9c61e1ae9c0d36f0816df8c53097d0e
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-25 23:58:15 +0530

    1st Program

---
+++ 25-march-2020/accpet_number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace accpet_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number till it non zero");
+            int positive = 0,min=0,max=0,avg=0;
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            while (num>0)
+            {
+                if (num > 0)
+                {
+                    positive++;
+                }
+                Console.WriteLine("Enter a number");
+                num = int.Parse(Console.ReadLine());
+                if(num<positive)
+                {
+                    min= num;
+                }
+                if (num > positive)
+                {
+                    max = num;
+                }
+                avg = (num+=num)/positive;
+            }
+            Console.WriteLine("Total Positive Number is : "+positive);
+            Console.WriteLine("Total Minimun Number is : " + min);
+            Console.WriteLine("Total Maximun Number is : " + max);
+            Console.WriteLine("Total avg Number is : " + avg);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 25-march-2020/calWindows/calWindows/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalLibrary;
+namespace calWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Class1 c = new Class1();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+            int result = c.Addition(num1, num2);
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+            int result = c.Subtraction(num1, num2);
+            textBox3.Text = result.ToString();
+        }
+    }
+}
