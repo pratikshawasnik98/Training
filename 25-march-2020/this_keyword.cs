0549bf5bbf1a0c27af162d9efe404c4772e9fda6
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:18:09 +0530

    30th Program

---
+++ 25-march-2020/this_keyword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace this_keyword
+{
+    class keyword
+    {
+        string name;
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public void show()
+        {
+            Console.WriteLine("Name :" + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            keyword obj = new keyword();
+            obj.SetName("Abc");
+            obj.show();
+            Console.ReadKey();
+        }
+    }
+}
