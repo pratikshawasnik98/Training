59a4aafef28249a6961e23b8cfa4a2afa8cecd29
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:06:54 +0530

    10th Program

---
+++ 25-march-2020/CalLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalLibrary
+{
+    public class Class1
+    {
+        public int Addition(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+        public int Subtraction(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+    }
+}
