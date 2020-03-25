1dbeb449285259e4c4d8146ce3c037d59b1bf430
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:13:27 +0530

    23rd Program

---
+++ 25-march-2020/nested_class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_class
+{
+
+    class BankAccount
+    {
+        public class Account
+        {
+            static string name;
+            public void SetName(string n)
+            {
+                name = n;
+                Console.WriteLine("Name :" + name);
+            }
+        }
+            public int actno;
+            public void SetActno(int a)
+            {
+                actno = a;
+                Console.WriteLine("Account  :" + actno);
+            }
+     }
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                BankAccount.Account obj = new BankAccount.Account();
+                obj.SetName("Abc");
+
+                BankAccount b = new BankAccount();
+                b.SetActno(22);
+                Console.ReadKey();
+            }
+        }
+}
+
