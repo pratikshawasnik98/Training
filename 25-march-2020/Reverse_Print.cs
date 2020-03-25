cc5fa026a860c0c836ffcee1b817f314e95a6ad4
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 00:14:10 +0530

    25th Program

---
+++ 25-march-2020/Reverse_Print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reverse_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int d, res = 0;
+            while(num!=0)
+            {
+                d = num % 10;
+                num = num / 10;
+                res = res*10 + d;
+                
+            }
+            Console.WriteLine("Reverse of number : " + res);
+            Console.ReadKey();
+        }
+    }
+}
