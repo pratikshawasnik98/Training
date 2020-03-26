d3b71c77464d7b2be30195f6807db74f27367be6
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 22:57:55 +0530

    9th Program

---
+++ 26-march-2020/positive_sum_of_it.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace positive_sum_of_it
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a 5 numbers:");
+            int[] num = new int[5];
+
+            for(int i=0;i<5;i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            int count = 0;
+            int res = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if(num[i]>0)
+                {
+                    count++;
+                    res = res + num[i];
+                }
+            }
+            Console.WriteLine("Result : " + res);
+            //Console.WriteLine(count);
+            float avg = res / count;
+            Console.WriteLine("Result Avrage : " + avg);
+            Console.ReadKey();
+        }
+    }
+}
