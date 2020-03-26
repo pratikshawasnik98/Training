0f29a320ecbebe66a46a5ef5b2d4e88957421b7b
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:04:13 +0530

    22th Program

---
+++ 26-march-2020/this_keyword_with_static_var.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace this_keyword_with_static_var
+{
+    class keyword
+    {
+        static string name;
+        public void SetName(string name)
+        {
+            keyword.name = name;
+        }
+        public void show()
+        {
+            Console.WriteLine("Name :" + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            keyword obj = new keyword();
+            obj.SetName("Abc");
+            obj.show();
+            Console.ReadKey();
+        }
+    }
+}
