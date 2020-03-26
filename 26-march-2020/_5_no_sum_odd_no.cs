1fa54d29e1a886339bb9aadc2e58edc5a13803ae
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:00:33 +0530

    15th Program

---
+++ 26-march-2020/_5_no_sum_odd_no.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_no_sum_odd_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a five number : ");
+            int[] num = new int[5];
+
+            for(int i=0;i<5;i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            int res = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if(num[i]%2!=0)
+                {
+                    res = res + num[i];
+                }
+            }
+            Console.WriteLine("Print result :"+res);
+            Console.ReadKey();
+        }
+    }
+}
