cccdd09ed3f8273e9f5cce883ca04986044a5133
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 22:58:29 +0530

    11th Program

---
+++ 26-march-2020/add_even_1_to_given.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_even_1_to_given
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number till you want to get a square :");
+            int num = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= num)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("Number :" + i);
+                    int square = i * i;
+                    Console.WriteLine(" Number square :" + square);
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
