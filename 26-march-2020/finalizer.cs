8c52df38df9ff76b63689577ab014b6705258ac6
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 22:57:18 +0530

    7th Program

---
+++ 26-march-2020/finalizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalizer
+{
+    class car
+    {
+        public car()
+        {
+            Console.WriteLine("Con called");
+        }
+        ~car()
+        {
+            Console.WriteLine("DeCon called");
+            Console.ReadKey();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car c = new car();
+            Console.ReadKey();
+        }
+    }
+}
