7196c92c541f0e7170055372b452aefa612c6c06
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:07:52 +0530

    30th Program

---
+++ 26-march-2020/maximum_no_of_3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximum_no_of_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter three number");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if(num1>num2 && num1>num3)
+            {
+                Console.WriteLine(num1 + " Is greater");
+            }
+            else if (num2 > num1 && num2 > num3)
+            { 
+                Console.WriteLine(num2 + " Is greater");
+            }
+            else
+            { 
+                Console.WriteLine(num3 + " Is greater");
+            }
+            Console.ReadKey();
+        }
+    }
+}
