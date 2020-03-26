b9ed2f125301b45b8036026b5cc982c0349a8e3f
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:01:23 +0530

    18th Program

---
+++ 26-march-2020/overloading_constructor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloading_constructor
+{
+    public class employee
+    {
+        public  int salary;
+        public employee(int annualSalary)
+        {
+            salary = annualSalary;
+        }
+        public employee(int weeklySalary,int numberofWeek)
+        {
+            salary = weeklySalary * numberofWeek;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee e = new employee(500000);
+            Console.WriteLine("Annual Salary : " + e.salary);
+
+            employee e1 = new employee(7000,4);
+            Console.WriteLine("Annual Salary : " + e1.salary);
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
