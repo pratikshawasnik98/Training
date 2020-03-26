6bb2ff09cebef1f3b3764c0aa66a75b3a161d856
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 22:58:11 +0530

    10th Program

---
+++ 26-march-2020/count_negative_positive_no.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count_negative_positive_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a 5 numbers:");
+            int[] num = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            int positive = 0;
+            int negative = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (num[i] > 0)
+                {
+                    positive++;
+                }
+                else if(num[i] < 0)
+                {
+                    negative++;
+                }
+            }
+            Console.WriteLine("Positive Number Result : " + positive);
+            Console.WriteLine("Negative Number Result : " + negative);
+            Console.ReadKey();
+        }
+    }
+}
