e0d4881ed676ce98cee119de53ff23fa22d1671a
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:07:36 +0530

    29th Program

---
+++ 26-march-2020/cal_distance_bwt_two_points.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cal_distance_bwt_two_points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a 1st point numbers");
+            int x1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a 2st point numbers");
+            int y1 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+
+            int result = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+            Console.WriteLine("Result :" + result);
+            Console.WriteLine("Distance between two points :" + Math.Sqrt(result));
+
+            Console.ReadKey();
+        }
+    }
+}
