8d124458756bd458443d4bac730f550e85922107
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:01:08 +0530

    17th Program

---
+++ 26-march-2020/private_constructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace private_constructor
+{
+    public class Counter
+    {
+        
+        private Counter()
+        {
+        }
+        public static int currentCount;
+        public static int incrementCount()
+        {
+            return ++currentCount;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter.currentCount = 100;
+            Counter.incrementCount();
+            Console.WriteLine("New count = {0}", Counter.currentCount);
+
+            Console.ReadKey();
+        }
+    }
+}
