254eabf3e9c10e676e0e663878ed1a1d771635fd
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 22:58:46 +0530

    12th Program

---
+++ 26-march-2020/print_even_1_to_50.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_even_1_to_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i<=50)
+            {
+                if(i%2==0)
+                {
+                    Console.WriteLine("Number :"+i);
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
