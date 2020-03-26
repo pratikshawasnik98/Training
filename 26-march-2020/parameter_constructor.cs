c4f52392731e292bcfadc45c115be48584171038
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:03:02 +0530

    21th Program

---
+++ 26-march-2020/parameter_constructor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parameter_constructor
+{
+    class person
+    {
+        private string last;
+        private string first;
+
+        public person(string firstName, string lastName)
+        {
+            last = lastName;
+            first = firstName;
+        }
+        public void show()
+        {
+            Console.WriteLine("First :" + first);
+            Console.WriteLine("Last :" + last);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a First Name :" );
+            string f = Console.ReadLine();
+
+            Console.WriteLine("Enter a Last Name :");
+            string l = Console.ReadLine();
+
+            person p = new person(f,l);
+            p.show();
+            Console.ReadKey();
+        }
+    }
+}
