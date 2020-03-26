c5dc69f33c8e9568ca2d69ce642efee1ce81bd15
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:07:17 +0530

    28th Program

---
+++ 26-march-2020/avrage_weight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avrage_weight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Weight 1 and  Weight 2: ");
+            float w1 = float.Parse(Console.ReadLine());
+            float w2 = float.Parse(Console.ReadLine());
+
+           // Console.WriteLine("Enter Weight 2: ");            
+
+            Console.WriteLine("Enter Item 1 Purchase: ");
+            float i1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Item 2 Purchase: ");
+            float i2 = float.Parse(Console.ReadLine());
+
+           // float weight_sum = w1 + w2;
+
+            float item_sum = i1 + i2;
+            float avg = item_sum / 2;
+
+            Console.WriteLine("Avrage :" + avg);
+            Console.ReadKey();
+        }
+
+    }
+}
