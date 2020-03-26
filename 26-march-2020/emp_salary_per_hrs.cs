3b32dbc8f45d756821d6be704039814f2b58c52e
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:05:28 +0530

    26th Program

---
+++ 26-march-2020/emp_salary_per_hrs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emp_salary_per_hrs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a emp ID :");
+            int emp_id = int.Parse(Console.ReadLine());
+            int hrs = 200;
+            int months = 26;
+            int total=months * 8;
+            int salary = total * hrs;
+
+            Console.WriteLine("Employee ID : " + emp_id + " Salaray :" + salary);
+            Console.ReadKey();
+        }
+    }
+}
