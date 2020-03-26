902b4748d08db66c4e570448f69cecead12c2cee
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:01:47 +0530

    19th Program

---
+++ 26-march-2020/static_construtor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_construtor
+{
+    public class adult
+    {
+        private static int minimumAge;
+        static adult()
+        {
+            minimumAge = 18;
+        }
+        public void show()
+        {
+            Console.WriteLine("Age = " + minimumAge);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            adult a = new adult();
+            a.show();
+            Console.ReadKey();
+        }
+    }
+}
