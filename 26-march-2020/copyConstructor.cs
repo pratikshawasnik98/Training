0c878ad0a8ac6c6f69f6ff527110364ed48aaea7
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 22:56:07 +0530

    4th Program

---
+++ 26-march-2020/copyConstructor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace copyConstructor
+{
+     class company
+    { 
+      public int emp_no;
+      public string emp_name;
+      public float emp_salary;
+
+      public company(int eno, string n, float sal)
+      {
+          emp_no = eno;
+          emp_name = n;
+          emp_salary = sal;
+      }
+      public company(company com)
+      {
+          emp_no = com.emp_no;
+          emp_name = com.emp_name;
+          emp_salary = com.emp_salary;
+      }
+      public void instance_show_detail()
+      {
+          Console.WriteLine("Instance Constructor");
+          Console.WriteLine("\nName : " + emp_name + "\nEmployee Id : " + emp_no + "\nEmployee Salary :" + emp_salary);
+      }
+      public void copy_show_detail()
+      {
+          Console.WriteLine("Copy Constructor");
+          Console.WriteLine("\nName : " + emp_name + "\nEmployee Id : " + emp_no + "\nEmployee Salary :" + emp_salary);
+      }
+
+  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Enter a Employee ID :");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter a Employee Name :");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter a Employee Salary :");
+                float salary = int.Parse(Console.ReadLine());
+
+                company c = new company(id, name, salary);
+                c.instance_show_detail();
+
+                company c1 = new company(c);
+                c1.copy_show_detail();
+
+                Console.ReadKey();
+
+            }
+        }
+}
