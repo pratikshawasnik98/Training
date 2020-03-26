33bbb0a6dd8437bdf04f7e7a59eb6d09c8782223
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:04:43 +0530

    24th Program

---
+++ 26-march-2020/bhaskaras_formula.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bhaskaras_formula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a three values :");
+            float a = float.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+            float c = float.Parse(Console.ReadLine());
+            float t = (b * b) - 4 * a * c;
+            Console.WriteLine("Result :" + t);
+            if (t >= 0)
+            {
+                Double temp = Math.Sqrt(t);
+                Console.WriteLine("Result :" + temp);
+            }
+            else
+            {
+                Console.WriteLine("It is not possible to find the roots ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
