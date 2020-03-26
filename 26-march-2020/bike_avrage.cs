2ca468bf4b89dab6f01357c23a684126c0b01798
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-26 23:07:02 +0530

    27th Program

---
+++ 26-march-2020/bike_avrage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bike_avrage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter distance:");
+            float distance = float.Parse(Console.ReadLine());
+
+            float avg;
+
+            Console.WriteLine("enter a fuel:");
+            float lit = float.Parse(Console.ReadLine());
+
+            avg = distance / lit;
+            Console.WriteLine("Avrage :" + avg);
+            Console.ReadKey();
+
+        }
+    }
+}
