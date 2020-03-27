1c18b1fa35f32b494b05fb977d82762fa647714d
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:51:47 +0530

    13th Program

---
+++ 27-march-2020/oop_int_and_char.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace oop_int_and_char
+{
+    class parentClass
+    {
+        public void first(int n,string name)
+        {
+            Console.WriteLine("Integer :" + n + " String :" + name);
+        }
+        public void secound(string name,int n)
+        {
+            Console.WriteLine("String :" + name + " Integer :" + n);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parentClass pc = new parentClass();
+            pc.first(2, "ABC");
+            pc.secound("PQR", 4);
+            Console.ReadKey();
+        }
+    }
+}
