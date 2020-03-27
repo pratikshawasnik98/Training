e7959b2627d905924e6a5e776b3765fff84b9434
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:57:55 +0530

    29th Program

---
+++ 27-march-2020/psw_crt_or_not.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psw_crt_or_not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+
+            Console.WriteLine("Enter a Password");
+            int psw = int.Parse(Console.ReadLine());
+
+            if(psw==1234)
+            {
+                Console.WriteLine("Enter a Password is correct");
+                goto end;
+            }
+            else
+            {
+                Console.WriteLine("Enter a Password is wrong");
+                goto start;
+
+            }
+            end:
+            Console.ReadKey();
+        }
+    }
+}
