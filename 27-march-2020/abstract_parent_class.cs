651c1deb0560e619f6fec15e11ce2dd2258bae41
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:54:57 +0530

    20th Program

---
+++ 27-march-2020/abstract_parent_class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace abstract_parent_class
+{
+    abstract class parent
+    {
+        public void message()
+        {
+            Console.WriteLine("Calling abstrct class");
+        }
+    }
+    class first:parent
+    {
+        public void message()
+        {
+            Console.WriteLine("Calling first class");
+        }
+    }
+    class second:parent
+    {
+        public void message()
+        {
+            Console.WriteLine("Calling secound class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            second sec = new second();
+            sec.message();
+          
+
+            first fir = new first();
+            fir.message();
+          
+            parent p = new first();
+            p.message();
+            Console.ReadKey();
+        }
+    }
+}
