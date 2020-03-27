925dc517b115d4c333b912930719ab76bb66c970
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:51:27 +0530

    12th Program

---
+++ 27-march-2020/oop_square_rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace oop_square_rectangle
+{
+    class Shape
+    {
+        public void SquareArea(float side)
+        {
+            float areaOfsquare = side * side;
+            Console.WriteLine("Area of Square :" + areaOfsquare);
+        }
+        public void RectangleArea(float lenght, float breadth)
+        {
+            float areaOfRectangle = lenght * breadth;
+            Console.WriteLine("Area of Rectangle :" + areaOfRectangle);
+        }
+        
+        
+    }
+    
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape area = new Shape();
+            area.SquareArea(4);
+            area.RectangleArea(5, 6);
+            Console.ReadKey();
+        }
+    }
+}
