7910e685c0b3c552d390f3fd78d0acb1b25f6ea8
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:50:34 +0530

    9th Program

---
+++ 27-march-2020/oop_degree_class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace oop_degree_class
+{
+    class Degree
+    {
+        public void getDegree()
+        {
+            Console.WriteLine("I Got a Degree");
+        }
+    }
+    class Undergraduate:Degree
+    {
+        public void getDegree()
+        {
+            Console.WriteLine("I am an Undergraduate");
+        }
+    }
+    class Postgraduate : Degree
+    {
+        public void getDegree()
+        {
+            Console.WriteLine("I am an Postgraduate");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Degree degree = new Degree();
+            degree.getDegree();
+
+            Undergraduate undergraduate = new Undergraduate();
+            undergraduate.getDegree();
+
+            Postgraduate postgraduate = new Postgraduate();
+            postgraduate.getDegree();
+
+            Console.ReadKey();
+        }
+    }
+}
