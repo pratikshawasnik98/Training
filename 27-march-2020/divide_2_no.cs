b0e9b795fbd8d49a623698bc262f7b868486b7e7
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:57:20 +0530

    26th Program

---
+++ 27-march-2020/divide_2_no.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divide_2_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Enter a 1 st number");
+                float num1 = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter a 2 sd number");
+                float num2 = float.Parse(Console.ReadLine());
+
+                if(num1%num2==0)
+                {
+                    Console.WriteLine(num1 + " is Divide by " + num2);
+                }
+                else
+                {
+                    Console.WriteLine(num1 +" is Not Divide by "+num2);
+                    
+                }
+            Console.ReadKey();
+            
+        }
+    }
+}
