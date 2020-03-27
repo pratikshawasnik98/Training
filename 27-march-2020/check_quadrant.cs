7dba29880e42588d51eba8e44af5c694ba5da555
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:57:35 +0530

    27th Program

---
+++ 27-march-2020/check_quadrant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_quadrant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a quadrant");
+            float x = float.Parse(Console.ReadLine());
+            float y = float.Parse(Console.ReadLine());
+
+            if(x > 0 && y > 0)
+            {
+                Console.WriteLine("Enter a quadrant points is in 1st quadrant");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Enter a quadrant points is in 2sd quadrant");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Enter a quadrant points is in 3rd quadrant");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Enter a quadrant points is in 4th quadrant");
+            }
+            Console.ReadKey();
+        }
+    }
+}
