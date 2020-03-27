1ca516bd1fcd00f50a61219d8f91c9267195dffb
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:49:36 +0530

    7th Program

---
+++ 27-march-2020/oop_parent_i_j.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace oop_parent_i_j
+{
+    class parent
+    {
+        public int i { get; set; }
+        public void printNum(int I)
+        {
+            i = I;
+            Console.WriteLine("I: " + i);
+        }
+    }
+    class subClass:parent
+    {
+        public int j { get; set; }
+        public void printNum(int J)
+        {
+            j = J;
+            Console.WriteLine("J: " + j);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            subClass sc = new subClass();
+            sc.printNum(10);
+            sc.printNum(20);
+            Console.ReadKey();
+        }
+    }
+}
