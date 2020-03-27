b0d6eea7af5bd50ff3e94551bbbaaecc2d37660e
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:52:03 +0530

    14th Program

---
+++ 27-march-2020/oops_primeNumber_class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace oops_primeNumber_class
+{
+    class primeNumber
+    {
+        public void print(int number1)
+        {
+            Console.WriteLine("Print method with integer parameter :" + number1);
+        }
+        public void print(float number2)
+        {
+            Console.WriteLine("Print method with float parameter :" + number2);
+        }
+        public void print(double number3)
+        {
+            Console.WriteLine("Print method with double parameter :" + number3);
+        }
+        public void print(string name)
+        {
+            Console.WriteLine("Print method with string parameter :" + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            primeNumber primenumber = new primeNumber();
+            primenumber.print(5);
+            primenumber.print(5.6);
+            primenumber.print(54.90);
+            primenumber.print("Hello");
+            Console.ReadKey();
+        }
+    }
+}
