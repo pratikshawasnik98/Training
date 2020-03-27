c5f4b479870722af5ca837e2d8de594c992fe8d9
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:54:41 +0530

    19th Program

---
+++ 27-march-2020/abstract_bank_class.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace abstract_bank_class
+{
+    abstract class Bank
+    {
+        public abstract void getBalance();
+       /* {
+            Console.WriteLine("$100 depoistes in Bank A");
+        }*/
+    }
+    class BankA : Bank
+    {
+        public override void getBalance()
+        {
+            Console.WriteLine("$100 depoistes in Bank A");
+        }
+    }
+    class BankB : Bank
+    {
+        public override void getBalance()
+        {
+            Console.WriteLine("$150 depoistes in Bank B");
+        }
+    }
+    class BankC : Bank
+    {
+        public override void getBalance()
+        {
+            Console.WriteLine("$200 depoistes in Bank C");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankA banka = new BankA();
+            banka.getBalance();
+
+            BankB bankb = new BankB();
+            bankb.getBalance();
+
+            BankC bankc= new BankC();
+            bankc.getBalance();
+
+            Console.ReadKey();
+        }
+    }
+}
