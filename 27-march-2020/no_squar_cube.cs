5e5456e4ed1c86c0d2c35e355a290f1ef5d07e4c
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:55:53 +0530

    23rd Program

---
+++ 27-march-2020/no_squar_cube.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no_squar_cube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number :");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number of lines you wnat to print :");
+            int no_of_lines = int.Parse(Console.ReadLine());
+
+            int square = num * num;
+            int cube = num * num * num;
+            for (int a = 1; a <= no_of_lines; a++)
+            {
+                Console.WriteLine(num +"Square is : " + square + " Cube is : " +cube);
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
