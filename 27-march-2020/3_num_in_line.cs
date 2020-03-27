0b0ee5763728d14f2d12a12dd934b026520aca12
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:56:06 +0530

    24th Program

---
+++ 27-march-2020/3_num_in_line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_num_in_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number you wnat to print from 1 to .....");
+            string num = Console.ReadLine();
+
+            Console.WriteLine("Enter a number of lines you wnat to print from 1 to .....");
+            int n = int.Parse(Console.ReadLine()) ;
+
+            for (int a = 1; a <= n; a++)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine();
+            
+            Console.ReadKey();
+        }
+    }
+}
