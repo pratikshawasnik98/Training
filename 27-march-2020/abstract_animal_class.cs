b0f990f1b8e485649fafde9fff35a47833cc3e0a
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:50:55 +0530

    10th Program

---
+++ 27-march-2020/abstract_animal_class.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace abstract_animal_class
+{
+    abstract class animal
+    {
+        abstract public void cats();
+        abstract public void dogs();
+    }
+    class Cats : animal
+    {
+        public override void cats()
+        {
+            Console.WriteLine("Cats Meow from cats class");
+        }
+        public override void dogs()
+        {
+            Console.WriteLine("Dogs Bark from cats class");
+        }
+    }
+    class Dogs : animal
+    {
+        public override void cats()
+        {
+            Console.WriteLine("Cats Meow from dogs class");
+        }
+        public override void dogs()
+        {
+            Console.WriteLine("Dogs Bark from dogs class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            animal cat = new Cats();
+            cat.cats();
+
+           animal dog = new Dogs();
+            dog.dogs();
+
+            Console.ReadKey();
+        }
+    }
+}
