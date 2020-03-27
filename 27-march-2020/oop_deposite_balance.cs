43110e9741029916beeed1b8b1f6251f55ca2cbd
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:49:49 +0530

    8th Program

---
+++ 27-march-2020/oop_deposite_balance.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace oop_deposite_balance
+{
+    class Bank
+    {
+        public int getBalance()
+        {
+            return 0;
+        }
+    }
+    class BankA:Bank
+    {
+        public int getBalance(int a)
+        {
+            return a;
+        }
+    }
+    class BankB : Bank
+    {
+        public int getBalance(int b)
+        {
+            return b;
+        }
+    }
+    class BankC : Bank
+    {
+        public int getBalance(int c)
+        {
+            return c;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            int parent=bank.getBalance();
+            Console.WriteLine("Bank Parent Class:" + parent);
+
+            BankA A = new BankA();
+            int a=A.getBalance(1000);
+            Console.WriteLine("Bank A:" + a);
+
+            BankB B = new BankB();
+            int b=B.getBalance(1500);
+            Console.WriteLine("Bank B:" + b);
+
+            BankC C = new BankC();
+            int c=C.getBalance(2000);
+            Console.WriteLine("Bank C:" + c);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
