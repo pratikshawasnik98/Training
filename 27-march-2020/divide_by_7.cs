fd3dc9f9c014e12c5c93b7e16d0e0bc39330352d
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:56:20 +0530

    25th Program

---
+++ 27-march-2020/divide_by_7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divide_by_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number range from");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter 2sd number range to");
+            float num2 = float.Parse(Console.ReadLine());
+
+            
+            while (num1 <= num2)
+            {
+                if (num1 % 7 == 2 )
+                {
+                    Console.WriteLine("number is divide by 7 with reminder 2 : " + num1);
+                }
+                if (num1 % 7 == 3)
+                {
+                    Console.WriteLine("number is divide by 7 with reminder 3 : " + num1);
+                }
+                num1++;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
