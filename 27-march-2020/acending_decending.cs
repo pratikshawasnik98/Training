02ec6ab6a51d0aebc18b198ccf27039175ccc685
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:58:10 +0530

    30th Program

---
+++ 27-march-2020/acending_decending.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acending_decending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a 1st number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a 2sd number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if(num1<num2)
+            {
+                Console.WriteLine("Enter numbers are in accending order");
+            }
+            else if (num1>num2)
+            {
+                Console.WriteLine("Enter numbers are in decending order");
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("Enter numbers are equal");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
