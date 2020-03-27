7abaa2ff4f93e19b7f6ff58121f30d808adbc593
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:56:35 +0530

    28th Program

---
+++ 27-march-2020/no_divide_by_17.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no_divide_by_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number range from");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter 2sd number range to");
+            float num2= float.Parse(Console.ReadLine());
+
+            float sum = 0;
+            while(num1<=num2)
+            {
+                if(num1%17!=0)
+                    {
+                        sum = sum + num1;
+                    }
+                num1++;
+            }
+            Console.WriteLine("Addition of number is : " + sum);
+            Console.ReadKey();
+        }
+    }
+}
