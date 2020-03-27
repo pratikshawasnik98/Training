3359468fb5257dd40850a7b31c8b36cd03b3cb51
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:55:15 +0530

    21th Program

---
+++ 27-march-2020/cal_avg_of_sub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cal_avg_of_sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sub_mark = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter a subject mark");
+                sub_mark[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                
+            }
+        }
+    }
+}
