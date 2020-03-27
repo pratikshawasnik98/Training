65c299a006fa4a42690e90743f78d2c48794c921
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:55:31 +0530

    22th Program

---
+++ 27-march-2020/print_no_in_a_no_of_lines.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_no_in_a_no_of_lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number :");
+             int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number of lines you wnat to print :");
+            int no_of_lines = int.Parse(Console.ReadLine());
+            
+            for (int a = 1; a <= no_of_lines; a++)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
