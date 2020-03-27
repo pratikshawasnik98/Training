b0cce22234be586902d90f85d2de221bd69cab87
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:49:19 +0530

    6th Program

---
+++ 27-march-2020/oop_static_class_.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace oop_static_class_
+{
+    class A
+    {
+        public static void method()
+        {
+            Console.WriteLine("Parent");
+        }
+    }
+    class B:A
+    {
+        public static void method()
+        {
+            Console.WriteLine("Child");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            A.method();
+
+            A b = new B();
+            //b.method();
+
+            B obj = new B();
+            B.method();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
