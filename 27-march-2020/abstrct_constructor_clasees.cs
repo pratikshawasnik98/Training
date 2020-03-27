72aaf073801acce3b50913f39f2bab03ebd395ac
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-27 22:54:16 +0530

    17th Program

---
+++ 27-march-2020/abstrct_constructor_clasees.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace abstrct_constructor_clasees
+{
+    abstract class parentClass
+    {
+        public parentClass()
+        {
+            Console.WriteLine("This is Constructor of abstract class");
+        }
+        abstract public void a_method();
+        public void normalMethod()
+        {
+            Console.WriteLine("This is normal method of abstrct class ");
+        }
+    }
+    class subClass : parentClass
+    {
+        public override void a_method()
+        {
+            Console.WriteLine("This is abstract method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            subClass subclass = new subClass();
+            subclass.a_method();
+            subclass.normalMethod();
+            Console.ReadKey();
+        }
+    }
+}
