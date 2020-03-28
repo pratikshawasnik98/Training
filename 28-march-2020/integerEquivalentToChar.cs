17b4e5eea61806d4be09cbac4e30fe14f31f67d8
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-29 00:22:42 +0530

    integer_equivalent_to_char

---
+++ 28-march-2020/integerEquivalentToChar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace integerEquivalentToChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            string letters = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int n;
+            Console.WriteLine("List of integer equivalents of letters (a-z, A-Z).\n");
+            for (n = 0; n < 53; n++)
+            {
+                Console.WriteLine(letters[n]);
+                Console.Write("");
+                /*if ((n + 1) % 6 == 0)
+                {
+                    
+                }*/
+            }
+        }
+    }
+}
