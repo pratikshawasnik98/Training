8e097fa5c26d081356c329cfa93e80a1e6135831
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-29 00:16:35 +0530

    remove_negativesign

---
+++ 28-march-2020/remove_negativeSign.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace remove_negativeSign
+{
+    class removeNegative
+    {
+        public void remove(float number)
+        {
+            if(number<0)
+            {
+                number = number * (-1);
+            }
+            Console.WriteLine("Posistive Value is : " + number);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a negative Number");
+            float negativeNumber = float.Parse(Console.ReadLine());
+
+            removeNegative removenegative = new removeNegative();
+            removenegative.remove(negativeNumber);
+
+            Console.ReadKey();
+
+        }
+    }
+}
