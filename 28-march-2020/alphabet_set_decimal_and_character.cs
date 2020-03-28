91d26e1f52baf9ea9c63f28797d8d052c3c0e286
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-29 00:08:12 +0530

    decimal_to_char

---
+++ 28-march-2020/alphabet_set_decimal_and_character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace alphabet_set_decimal_and_character
+{
+    /*class decimalToCharacter
+    {
+        public void method(int num)
+        {
+            string Name = num.ToString();
+            for (num = 65; num <= 122; Name = Name + 1)
+            {
+                if (num > 90 && num < 97)
+                    continue;
+                Console.WriteLine("Character :" + Name);               
+            }
+            Console.WriteLine();
+        }
+    }*/
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            decimalToCharacter obj = new decimalToCharacter();
+            obj.method(number);
+            */
+
+            char c;
+            //int temp = int.Parse(c);
+            Console.WriteLine("\n\n");
+            for (c = 65 ; c <= 122; c = c + 1)
+            {
+                if (c > 90 && c < 97)
+                    continue;
+                Console.WriteLine(" ", c, c);
+            }
+            Console.WriteLine("|\n");
+            Console.ReadKey();
+        }
+    }
+}
