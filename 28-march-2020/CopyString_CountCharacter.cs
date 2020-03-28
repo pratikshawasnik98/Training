03e8e05c0d683a4b7602f1c81e12989caa26ef2f
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-29 00:15:43 +0530

    copy_count_character

---
+++ 28-march-2020/CopyString_CountCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CopyString_CountCharacter
+{
+    class CopyString
+    {
+        public string copyStringName;
+        public void copy(string name)
+        {
+            copyStringName = name;
+                Console.WriteLine("Print copy Name : " + copyStringName);
+                Console.WriteLine("Print the character is to be copied : " + copyStringName.Length);
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringName;
+             Console.WriteLine("Enter a string ");
+              stringName = Console.ReadLine();
+            CopyString copyObj = new CopyString();
+            copyObj.copy(stringName);
+            Console.ReadKey();
+        }
+    }
+}
