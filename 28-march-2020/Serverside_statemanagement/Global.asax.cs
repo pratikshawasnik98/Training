65d4d4cff89baa82c9e9f9033015fe5a7c4e92b9
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 00:29:22 +0530

    .cs files

---
+++ 28-march-2020/Serverside_statemanagement/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Serverside_statemanagement
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["Companyname"] = "MKPlacement";
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session["username"] = "abc";
+            Response.Write("<br>session is now begining");
+            Response.Write("<br>session variable value : " + Session["username"].ToString());
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Response.Write("request is now beging");
+            Response.Write("<br>Company Name : " + Application["companyname"].ToString());
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 28-march-2020/Serverside_statemanagement/design1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Serverside_statemanagement
+{
+    public partial class design1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<br>page load event fire");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("design1.aspx?uname=" + TextBox1.Text);
+        }
+    }
+}
