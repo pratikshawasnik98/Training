5beb0b8402717a6b0b018f43d15b942a9d4a11fe
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 00:25:14 +0530

    .cs files

---
+++ 28-march-2020/Mkplacement_website/register.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Mkplacement_website
+{
+    public partial class register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cn = Application["companyname"].ToString();
+            string ad = Application["address"].ToString();
+            int cnt = Convert.ToInt32(Application["counter"].ToString());
+            cnt = cnt + 1;
+            Application["counter"] = cnt;
+            Response.Write("welcome to " + cn + "<Br>");
+            Response.Write("at  " + ad + "<Br>");
+            Response.Write("no of visitors : " + cnt.ToString() + "<Br>");
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["username"] = TextBox1.Text;
+            Session["email"] = TextBox2.Text;
+            Label1.Text = "registered successfully";
+
+        }
+    }
+}
---
+++ 28-march-2020/Mkplacement_website/viewInfo.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Mkplacement_website
+{
+    public partial class viewInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string u = Session["username"].ToString();
+            string em = Session["email"].ToString();
+            int uc = Convert.ToInt32(Session["usercount"].ToString());
+            uc = uc + 1;
+            Session["usercount"] = uc;
+            Label1.Text = u;
+            Label2.Text = em;
+            Label3.Text = uc.ToString();
+
+        }
+    }
+}
