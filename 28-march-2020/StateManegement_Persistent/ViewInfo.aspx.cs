af42769b8b2678b52aeee74c71043a6448c2f607
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 00:20:40 +0530

    .cs Program

---
+++ 28-march-2020/StateManegement_Persistent/ViewInfo.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateManegement_Persistent
+{
+    public partial class ViewInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
