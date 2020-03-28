92d005d60271b4e64cc640359187efd39bb37952
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-03-29 00:23:33 +0530

    integer_multiply_check

---
+++ 28-march-2020/integerMultiplyCheck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace integerMultiplyCheck
+{
+    class multiply
+    {
+        public void integerMutiply(int num1,int num2)
+        {
+            if(num1/num2==0)
+            {
+                Console.WriteLine(num1 + " Is Completly multiply by " + num2);
+            }
+            else
+            {
+                Console.WriteLine(num1 + " Is not Completly multiply by " + num2);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a first integer");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a secound integer");
+            int number2 = int.Parse(Console.ReadLine());
+
+            multiply multi = new multiply();
+
+            multi.integerMutiply(number1, number2);
+
+            Console.ReadKey();
+        }
+    }
+}
