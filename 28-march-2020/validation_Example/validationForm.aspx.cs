465a84b348c2db53c8f505b30c019cf4a4e82ee5
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 00:11:59 +0530

    ,cs files

---
+++ 28-march-2020/validation_Example/validationForm.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace validation_Example
+{
+    public partial class validationForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
