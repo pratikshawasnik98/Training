5353f6249ec4a537b98fd797d3e90e08b7ec9121
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 22:51:41 +0530

    .cs files

---
+++ 29-march-2020/ServerObjectExecuteMethod/ExecuteMethod.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerObjectExecuteMethod
+{
+    public partial class ExecuteMethod : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<br>Home Page Execution (code before calling execute method )");
+
+            Server.Execute("next.aspx");
+           Response.Write("<br> (code after calling execute method)");
+
+        }
+    }
+}
