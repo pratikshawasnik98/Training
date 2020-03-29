8991d7a170aff38d94fc58ea1cb2085550ea22c4
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 23:04:21 +0530

    .cs files

---
+++ 29-march-2020/ServerObjectTransferMethod/HomePage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerObjectTransferMethod
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<br>Home Page Execution (code before calling execute method )");
+
+            Server.Transfer("NextPage.aspx");
+            //Response.Write("<br> (code after calling execute method)");
+        }
+    }
+}
