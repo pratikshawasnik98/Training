6ca1cc535aa41f1428e2e6a94617f37735c270da
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 22:42:11 +0530

    .cs files

---
+++ 29-march-2020/serverMapPathExample/mapPath.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace serverMapPathExample
+{
+    public partial class mapPath : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write(Server.MapPath("/default5.aspx"));
+            Response.Write("<br>");
+            Response.Write(Server.MapPath(Request.ServerVariables.Get("PATH_INFO")));
+
+
+        }
+    }
+}
