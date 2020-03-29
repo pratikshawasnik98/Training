b9f22c4b6d2f5ff79039a191f0b1307205c08b9b
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 22:34:35 +0530

    .cs files

---
+++ 29-march-2020/exampleOfURLEncode/after.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace example_Of_URLEncode
+{
+    public partial class after : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["name"].ToString() != null)
+            {
+                string name = Request.QueryString["name"].ToString();
+                Response.Write("name = " + name);
+            }
+            if (Request.QueryString["password"].ToString() != null)
+            {
+                string pass = Request.QueryString["password"].ToString();
+                Response.Write("password  = " + pass);
+            }
+
+
+
+        }
+    }
+}
---
+++ 29-march-2020/exampleOfURLEncode/exampleURL.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace example_Of_URLEncode
+{
+    public partial class exampleURL : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String name = Server.UrlEncode("John Saunders");
+           // Response.Redirect("https://localhost:44394/after.aspx" + name);
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            String name = Server.UrlEncode("Joh'n /Saunders");
+            String password = Server.UrlEncode("king");
+            //query string to pass more than one value 
+            Response.Redirect("https://localhost:44394/after.aspx" + name + " &password=" + password);
+
+        }
+    }
+}
