211acb696b684513c2d8b085b965cb9824cec6b1
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-29 22:55:44 +0530

    .cs files

---
+++ 29-march-2020/ServerObjectHTMLEncodeMethod/htmlCode.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerObjectHTMLEncodeMethod
+{
+    public partial class htmlCode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text="<h1><u>welcome to Page which are without HTML Code</u></h1>";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Text = Server.HtmlEncode("<h1><u>welcome to Page with HTML Code</u></h1>");
+        }
+    }
+}
