613264784de243a5c5cd89e5826e135b1a9c1ffe
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-30 22:31:01 +0530

    calculateCubeAndSquare0To20.cs

---
+++ 30-march-2020/calculateCubeAndSquare0To20.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace calculateCubeAndSquare0To20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int square, cube;
+            Console.WriteLine("Square and Cube of 0 To 20 Numbers");
+
+            Console.WriteLine("Number    Square    Cube    ");
+            while (i<=20)
+            {
+                square = i * i;
+                cube = i * i * i;
+                Console.WriteLine(i+"          "+square + "          " + cube+"        " );
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
