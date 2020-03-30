d3f0f87dbfc3c5100401202cd064b7e394bf2d5d
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-30 22:29:15 +0530

    acceptSevenDigitNunberAndSeparateIt.cs

---
+++ 30-march-2020/acceptSevenDigitNunberAndSeparateIt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace acceptSevenDigitNunberAndSeparateIt
+{
+    class separate
+    {
+        public int digit;
+        public void separateNumber(int num)
+        {
+            for(int i=0;num!=0;i++)
+            {
+                digit = num % 10;
+                num = num / 10;
+                Console.Write(digit+ "  ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter seven digit number");
+            int number = int.Parse(Console.ReadLine());
+
+            separate sep = new separate();
+            sep.separateNumber(number);
+
+            Console.ReadKey();
+        }
+    }
+}
