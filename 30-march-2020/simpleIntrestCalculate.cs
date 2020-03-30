8cbf194fe109035ff265b55d92e57208b80f4841
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-30 22:32:41 +0530

    simpleIntrestCalculate.cs

---
+++ 30-march-2020/simpleIntrestCalculate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace simpleIntrestCalculate
+{
+    class simpleIntrest
+    {
+        public float finalAmount;
+        public void simple(float p,float r,float t)
+        {
+            finalAmount = p * (1 + r * t);
+            Console.WriteLine("Final Simple Intrest Amount : " + finalAmount);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Principle Balance");
+            float principle = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a Anunal Intrest Rate");
+            float rate = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a Time(In years)");
+            float time = float.Parse(Console.ReadLine());
+
+            simpleIntrest si = new simpleIntrest();
+            si.simple(principle, rate, time);
+
+            Console.ReadKey();
+
+        }
+    }
+}
