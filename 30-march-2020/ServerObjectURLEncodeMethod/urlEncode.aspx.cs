12177b1c5602c35ff971751bd16ca3f8715cc128
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-30 22:20:40 +0530

    .cs files

---
+++ 30-march-2020/ServerObjectURLEncodeMethod/urlEncode.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerObjectURLEncodeMethod
+{
+    public partial class urlEncode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Label1.Text="https://www.google.com";
+            Response.Redirect("https://www.google.com");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "https://www.google.com";
+        }
+    }
+}
