91dd1631fd22d0b42ae6e507df084985dd1d17bb
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-03-30 22:24:10 +0530

    .cs files

---
+++ 30-march-2020/serverProperties/sessionID.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace serverProperties
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string sid = Session.SessionID;
+            // Response.Write("session id " + sid);
+            Session["sessionid"] = sid;
+            Response.Write("session id : " + Session["sessionid"].ToString());
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("sessionID.aspx");
+        }
+    }
+}
---
+++ 30-march-2020/serverProperties/sessionIDAfter.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace serverProperties
+{
+    public partial class sessionIDAfter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Session.IsNewSession)
+            {
+                Session["sessionid1"] = Session.SessionID;
+                Response.Write("new session<br>");
+
+                Response.Write("<br>");
+            }
+            else
+            {
+
+                Response.Write("continueing with same old seesion");
+            }
+
+            Response.Write("<br>session id " + Session["sessionid1"].ToString());
+            int to = Session.Timeout;
+            Response.Write("time out " + to);
+            int lid = Session.LCID;
+            Response.Write("lid " + lid);
+            int scount = Session.Count;
+            Response.Write("no of items in session " + scount.ToString());
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Session.Abandon(); // this will abandon the session and we request again it will //start a new session
+        }
+    }
+}
