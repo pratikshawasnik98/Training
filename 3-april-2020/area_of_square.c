3f4a1deae16498b46c22c645782d1c5b8919b368
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-03 22:52:52 +0530

    area_of_square

---
+++ 3-april-2020/area_of_square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int square ,side;
+
+    printf("Enter a side : \n");
+    scanf("%d",&side);
+
+    square=side*side;
+
+    printf("area of square : %d",square);
+
+    getch();
+}
+
+
