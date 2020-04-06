7ef35cafb5dc23f3492cfb476224ead047140551
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-06 23:42:57 +0530

    print_1_to_20_using_Dowhilec

---
+++ 6-april-2020/print_1_to_20_using_Dowhilec.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int number =1;
+    do
+    {
+        printf("number : %d \n",number);
+        number++;
+    }while(number<=20);
+    getch();
+}
