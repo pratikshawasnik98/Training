c6737a86e1194eea30998e48a883d4daf75704ba
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-07 23:15:21 +0530

    numberPattern2

---
+++ 7-april-2020/numberPattern2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int row,col;
+    for(row=3;row>=1;row--)
+    {
+        for(col=3;col>=1;col--)
+        {
+            printf("%d",col);
+        }
+        printf("\n");
+    }
+
+    getch();
+}
+
+
