1fb90693e6df591dc52d951e10ba98fb6553b9c6
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-07 23:14:16 +0530

    print_a_to_z_using_while

---
+++ 7-april-2020/print_a_to_z_using_while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i=97;
+    printf("List of charaster : \n");
+    while(i<=122)
+    {
+        printf(" %d : %c \n",i,i);
+        i++;
+    }
+    getch();
+}
