b5ae7622a9cbb364b25347e1ce42a2ca721d8f59
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-07 23:15:09 +0530

    numberPattern1

---
+++ 7-april-2020/numberPattern1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int row,col;
+    for(row=3;row>=1;row--)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("%d",col);
+        }
+        printf("\n");
+    }
+
+    getch();
+}
+
+
+
