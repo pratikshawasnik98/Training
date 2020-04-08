a80c2b54ede23271c42e4121592af971c81816fc
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-08 22:29:50 +0530

    starPattern1

---
+++ 8-april-2020/starPattern1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int row,col;
+    for(row=1;row<=3;row++)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    getch();
+}
+
+
