dbeee30b818b4c5019271d26abdbe6d98f96f600
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-09 22:46:13 +0530

    numberPattern9

---
+++ 9-april-2020/numberPattern9.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int row,col;
+    for(row=1;row<=3;row++)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("%d",col);
+        }
+        printf("\n");
+    }
+    for(row=2;row>=1;row--)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("%d",col);
+        }
+        printf("\n");
+
+    }
+
+    getch();
+}
+
+
+
+
