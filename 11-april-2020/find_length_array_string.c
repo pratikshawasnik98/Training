384504a31bda4376563e614b41ca2657a60431de
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-10 23:14:46 +0530

    find_length_array_string

---
+++ 11-april-2020/find_length_array_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char name[20];
+    int count=0;
+    printf("Enter a you name:\n");
+    scanf("%s",&name);
+
+    for(int i=0;name[i]!='\0';i++)
+    {
+           count++;
+    }
+    printf("Length of array name : %d",count);
+    getch();
+}
+
