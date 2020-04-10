26d9c445cf9441ccf6faf02be6a876ca0b53e8b4
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-10 23:16:00 +0530

    write_string_and_display

---
+++ 11-april-2020/write_string_and_display.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char name[20];
+    printf("Enter a you name:\n");
+    scanf("%s",&name);
+
+    printf("Name : %s",name);
+    getch();
+}
