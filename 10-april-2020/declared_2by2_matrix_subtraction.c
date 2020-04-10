99c8661e08640f124348d129c9d8dc8e6267c123
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-10 23:10:44 +0530

    declared_2by2_matrix_subtraction

---
+++ 10-april-2020/declared_2by2_matrix_subtraction.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[2][2]={{1,2},{5,6}};
+    int b[2][2]={{7,8},{4,5}},c[2][2];
+    printf("subtraction of two matrix: \n");
+       printf("subtraction of 2 matrix element : \n");
+    for(int i=0;i<=1;i++)
+    {
+        for(int j=0;j<=1;j++)
+        {
+                c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+   printf("2*2 Matrix subtraction is :\n");
+    for(int i=0;i<=1;i++)
+    {
+        for(int j=0;j<=1;j++)
+        {
+                printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+    getch();
+}
+
