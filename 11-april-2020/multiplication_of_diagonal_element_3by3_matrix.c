bd1a74582071a70f3f9ae41ccf9e5aeab797a796
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-10 23:13:58 +0530

    multiplication_of_diagonal_element_3by3_matrix

---
+++ 11-april-2020/multiplication_of_diagonal_element_3by3_matrix.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[3][3],i,c=1;
+    printf("Enter a 3*3 matrix element : \n");
+    for(i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+        {
+                scanf("%d",&a[i][j]);
+        }
+    }
+    printf("3*3 Matrix diagonal element is :\n");
+    for(i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+        {
+                printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("2*2 Matrix is :\n");
+    for(i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+        {
+                if(i==j)
+                    c=c*a[i][j];
+
+        }
+    }
+    printf("addition of diagonal element %d ",c);
+    getch();
+}
+
+
+
