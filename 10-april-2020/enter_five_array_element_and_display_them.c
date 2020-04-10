f97c4cdcf4047d09ee6c32732af863bfa5518960
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-10 23:05:13 +0530

    enter_five_array_element_and_display_them

---
+++ 10-april-2020/enter_five_array_element_and_display_them.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[5],i;
+    printf("Enter a five array element : \n");
+    for(i=0;i<=4;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=4;i++)
+    {
+        printf("\n%d ",a[i]);
+    }
+    getch();
+}
