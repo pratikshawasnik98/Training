b0193b37c7a85cef4c92bf86c40a2c8ce9aa5b3c
pratikshawasnik98 <pratikshawasnik98@gmail.com>
2020-04-14 22:21:44 +0530

    print_even_number_using_fun_noArg_noReturn.c

---
+++ 14-april-2020/print_even_number_using_fun_noArg_noReturn.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    myfun();
+    getch();
+}
+void myfun()
+{
+    for(int i=0;i<=9;i++)
+    {
+        if(i%2==0}
+        {
+            printf("Even Number :%d\n",i);
+        }
+    }
+}
+
