31941b88c6c9a51f2ea52eb74be861efac3b7a50
Pratiksha Wasnik <61428423+pratikshawasnik98@users.noreply.github.com>
2020-04-18 22:23:51 +0530

    starPattern_with_noArgur_noReturen

---
+++ 18-april-2020/starPattern_with_noArgur_noReturen.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    pattern();
+    getch();
+}
+void pattern()
+{
+    int i,j,rows;
+
+    printf("enter a rows : \n");
+    scanf("%d",&rows);
+
+    for(i=1;i<=rows;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
